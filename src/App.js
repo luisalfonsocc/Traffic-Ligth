25ffb7b1798e627c3446a285b707b6ccf5bb65ad
Luis Carreno <luisalfonsocc@gmail.com>
2023-11-14 12:39:23 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css'; // Asegúrate de importar el archivo CSS
 
 function App() {
+  const [redLight, setRedLight] = useState(false);
+  const [yellowLight, setYellowLight] = useState(false);
+  const [greenLight, setGreenLight] = useState(false);
+
+  const clickRedLight = () => {
+    setRedLight(true);
+    setYellowLight(false);
+    setGreenLight(false);
+  };
+
+  const clickYellowLight = () => {
+    setRedLight(false);
+    setYellowLight(true);
+    setGreenLight(false);
+  };
+
+  const clickGreenLight = () => {
+    setRedLight(false);
+    setYellowLight(false);
+    setGreenLight(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="semaforo-container">
+      <div className="soporte-superior"></div>
+    <div className="streetLightBg">
+      
+      <div
+        className={`bulb redBulb ${redLight ? 'bulbOn' : ''}`}
+        onClick={clickRedLight}
+      ></div>
+      <div
+        className={`bulb yellowBulb ${yellowLight ? 'bulbOn' : ''}`}
+        onClick={clickYellowLight}
+      ></div>
+      <div
+        className={`bulb greenBulb ${greenLight ? 'bulbOn' : ''}`}
+        onClick={clickGreenLight}
+      ></div>
+    </div>
     </div>
   );
 }
 
 export default App;
+
+
